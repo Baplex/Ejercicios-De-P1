9c98791e8b61a869cdffda356c01860b5acba5a7
Baplex <122424586+Baplex@users.noreply.github.com>
2023-02-13 20:40:59 -0500

    Add files via upload

---
+++ ROT13/Program.cs
@@ -0,0 +1,37 @@
+﻿List<string> abc = new List<string>();
+List<string> abcRot13 = new List<string>();
+List<string> final = new List<string>();
+char[] array;
+
+Console.WriteLine("Introduzca el codigo que desea decifrar: ");
+string ciph = Console.ReadLine();
+
+array = ciph.ToCharArray();
+
+for(char i = 'A'; i <= 'Z'; i++){
+    abc.Add(i.ToString());
+}
+
+for(char i = 'N'; i <= 'Z'; i++){
+    abcRot13.Add(i.ToString());
+}
+for(char i = 'A'; i <= 'M'; i++){
+    abcRot13.Add(i.ToString());
+}
+
+foreach(char e in array){
+string p = e.ToString();
+    if (e >= 'A' && e <= 'Z'){ 
+    int indx = abc.IndexOf(p);
+    string z = abcRot13[indx];
+    final.Add(z);
+    }
+    else{
+        final.Add(p);
+    }
+}
+
+char sp = ',';
+string resultado = String.Join(sp, final);
+string resultadof = resultado.Replace(",", "");
+Console.WriteLine(resultadof);
